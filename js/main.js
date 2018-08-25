73eadcac5ad19f8d518abb5026a281e2a793dc26
Pegas13fps <Tymoschenkob@gmail.com>
2018-08-26 00:07:24 +0300

    optimization code

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ document.getElementById("kmInput").addEventListener('input', function(e) {
     let km = e.target.value;
 
     document.getElementById("metresOutput").innerHTML = km * 1000;
-    document.getElementById("metresOutput").innerHTML = km * 1000;
-    document.getElementById("metresOutput").innerHTML = km * 1000;
+    document.getElementById("smOutput").innerHTML = km * 100000;
+    document.getElementById("milimetersOutput").innerHTML = km * 1000000;
 
     console.log(km);
 });

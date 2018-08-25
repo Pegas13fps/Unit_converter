ee34b9685b9f922268466916a6fb3e235b9d4b27
Pegas13fps <Tymoschenkob@gmail.com>
2018-08-26 00:13:07 +0300

    new functional visible

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-document.getElementById("kmInput").addEventListener('input', function(e) {
-    let km = e.target.value;
 
+// Спрячем output пока не активны
+document.getElementById('output').style.visibility = 'hiden';
+
+// Конвертер Км - м, см, мл.
+document.getElementById("kmInput").addEventListener('input', function(e) {
+    // При активном input отобразим поля.
+    document.getElementById('output').style.visibility = 'visible';
+
+    let km = e.target.value;
     document.getElementById("metresOutput").innerHTML = km * 1000;
     document.getElementById("smOutput").innerHTML = km * 100000;
     document.getElementById("milimetersOutput").innerHTML = km * 1000000;
